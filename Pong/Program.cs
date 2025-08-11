be64a76e3b5226eb176ea4643bcee1438391b2c0
EverlastingSlumber <223306172+xX-Melatonina-Xx@users.noreply.github.com>
2025-08-11 19:58:32 +0200

    Add files via upload

---
+++ Pong/Program.cs
@@ -0,0 +1,25 @@
+﻿using Pong.Controllers;
+using Pong.Models;
+using Pong.Views;
+using System;
+using System.Threading;
+
+namespace Pong
+{
+    class Program
+    {
+        static void Main()
+        {   
+            Console.SetWindowSize(33, 16);
+            Console.SetBufferSize(33, 16);
+            Console.Title = "Pong";
+            Console.CursorVisible = false;
+            
+            Thread Gameloop = new Thread(() =>GameController.StartGame());
+            Thread InputStream = new Thread(() => GameController.InputManager());
+
+            InputStream.Start();
+            Gameloop.Start();
+        }
+    }
+}
---
+++ Pong/Views/Interface.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pong.Views
+{
+    class Interface
+    {                                                                                                                                                   
+        public static List<string> interfaceLayer0 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer1 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer2 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer3 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer4 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer5 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " , " "};
+        public static List<string> interfaceLayer6 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer7 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer8 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer9 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+        public static List<string> interfaceLayer10 = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+
+
+
+        public static List<List<string>> Layers = new List<List<string>>
+        {
+            interfaceLayer0,
+            interfaceLayer1,
+            interfaceLayer2,
+            interfaceLayer3,
+            interfaceLayer4,
+            interfaceLayer5,
+            interfaceLayer6,
+            interfaceLayer7,
+            interfaceLayer8,
+            interfaceLayer9,
+            interfaceLayer10,
+        };
+
+
+        public static void DisplayGameArena(int scorePlayer, int scoreRival, string winner)
+        {
+            Console.WriteLine($"     {scorePlayer}                  {scoreRival}");
+            Console.WriteLine("==============================");
+                for (int i = 0; i < Layers.Count; i++)
+                {
+                    foreach (string pixel in Layers[i])
+                    {
+                        Console.Write(pixel);
+                    }
+
+                    Console.WriteLine();
+                }
+            Console.WriteLine("==============================");
+            Console.WriteLine($"       {winner}");
+        }
+
+
+        public static void SelectGameMode()
+        {
+            Console.WriteLine();
+            Console.WriteLine("   _____   ____  _   _  _____ ");
+            Console.WriteLine("  |  __ \\ / __ \\| \\ | |/ ____|");
+            Console.WriteLine("  | |__) | |  | |  \\| | |  __ ");
+            Console.WriteLine("  |  ___/| |  | | . ` | | |_ |");
+            Console.WriteLine("  | |    | |__| | |\\  | |__| |");
+            Console.WriteLine("  |_|     \\____/|_| \\_|\\_____|");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Wybierz tryb:");
+            Console.WriteLine("1) gracz vs gracz (W/S UP/DOWN)");
+            Console.WriteLine("2) gracz vs bot (W/S)");
+        }
+    }
+}                           
